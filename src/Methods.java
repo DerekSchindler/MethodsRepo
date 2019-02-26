283e949003c2a58b2940f250862ca02ab61e4f69
Derek S <derek.schindler.2022@studet.lhchools.org>
2019-02-26 07:55:59 -0500

    Created first method

---
+++ src/Methods.java
@@ -0,0 +1,12 @@
+public class Methods {
+
+    public static void main(String[] args){
+        //write code here
+        printGreeting();
+
+    }
+    public static void printGreeting(){
+        System.out.println("Hello World");
+
+    }
+}

ddad25e9bd807f8e28e0a7f669d2a4be151b9967
Derek S <derek.schindler.2022@studet.lhchools.org>
2019-03-07 07:47:37 -0500

    Created fourth and fifth methods (forgot to commit for fourth)

--- src/Methods.java
+++ src/Methods.java
@@ -8,18 +8,35 @@ public class Methods {
 
         System.out.println(multiply(5,2));
 
-        
+        System.out.println(divide(13.6,5.7));
 
+        max(3.5,10.75);
 
     }
     public static void printGreeting(){
         System.out.println("Hello World");
     }
     public static void greetings(String name){
-        System.out.println("Hello "+ name);
+        System.out.println("Hello "+name);
     }
     public static int multiply(int num1, int num2){
         int product = num1*num2;
         return product;
     }
+    public static double divide(double num1, double num2){
+        double answer = num1/num2;
+        return answer;
+    }
+    public static void max(double num1, double num2){
+        if(num1>num2){
+             System.out.println(num1+" is the larger number.");
+        }
+        else if(num1<num2){
+            System.out.println(num2+" is the larger number.");
+        }
+        else if(num1==num2){
+            System.out.println("The numbers are equal");
+        }
+    }
+    
 }
